a4e36a018260930a6423dad4adea1e9fdcfcd3a4
wilyos <wilyos99@gmail.com>
2025-05-16 10:03:48 -0500

    animations + responsive base

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const video2 = document.getElementById('projectVideo2');
 const video3 = document.getElementById('projectVideo3');
 const hoverSign = document.querySelector('.hover-sign');
 
+//side bar elements//
+const sideBar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const closeSideBar = document.querySelector('.close-icon');
+
 
 
 const videoList = [video1, video2, video3];
@@ -20,3 +25,15 @@ videoList.forEach((video) => {
   });
 } 
 )
+
+//sidebar//
+
+menu.addEventListener('click', () => { 
+  sideBar.classList.add('open-sidebar');
+  sideBar.classList.remove('close-sidebar');
+});
+
+closeSideBar.addEventListener('click', () => {
+  sideBar.classList.remove('open-sidebar');
+  sideBar.classList.add('close-sidebar');
+});
